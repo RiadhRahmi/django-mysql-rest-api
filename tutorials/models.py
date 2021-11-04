bfa5ea72ead2c3ec533ef46cc7bf0c8ecbe8b65d
Riadh Rahmi <rrahmi@elyosdigital.com>
2021-11-04 14:01:36 +0100

    update settings in admin panel

--- tutorials/models.py
+++ tutorials/models.py
@@ -6,3 +6,4 @@ class Tutorial(models.Model):
     title = models.CharField(max_length=70, blank=False, default='')
     description = models.CharField(max_length=200,blank=False, default='')
     published = models.BooleanField(default=False)
+    
